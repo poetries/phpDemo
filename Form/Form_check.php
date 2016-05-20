95ba4f67cf7868ad7e6e26b8880820c7472803c2
poetries <jingguanliuye@gmail.com>
2016-05-20 18:25:23 +0800

    update

--- Form/Form_check.php
+++ Form/Form_check.php
@@ -2,8 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        .error {color: #FF0000;}
+    </style>
 </head>
 <body>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -26,42 +30,81 @@
 将函数命名为 test_input()。
 现在，我们可以通过test_input()函数来检测 $_POST 中的所有变量, 脚本代码如下所示：*/
 
-// 定义变量并默认设置为空值
+
+
+/*我们在执行以上脚本时，会通过$_SERVER["REQUEST_METHOD"]来检测表单是否被提交 。
+如果 REQUEST_METHOD 是 POST, 表单将被提交 - 数据将被验证。如果表单未提交将跳过验证并显示空白。
+在以上实例中使用输入项都是可选的，即使用户不输入任何数据也可以正常显示*/
+
+// 定义变量并默认设为空值
+//在以下代码中我们加入了一些新的变量: $nameErr, $emailErr, $genderErr, 和 $websiteErr.
+//我们还为每个$_POST变量增加了一个if else语句。 这些语句将检查 $_POST 变量是 否为空
+//如果为空，将显示对应的错误信息。 如果不为空，数据将传递给test_input() 函数
+
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $name = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $website = test_input($_POST["website"]);
-    $comment = test_input($_POST["comment"]);
-    $gender = test_input($_POST["gender"]);
+
+    if (empty($_POST["name"]))
+    {$nameErr = "Name is required";}
+    else
+    {$name = test_input($_POST["name"]);}
+
+    if (empty($_POST["email"]))
+    {$emailErr = "Email is required";}
+    else
+    {$email = test_input($_POST["email"]);}
+
+    if (empty($_POST["website"]))
+    {$website = "";}
+    else
+    {$website = test_input($_POST["website"]);}
+
+    if (empty($_POST["comment"]))
+    {$comment = "";}
+    else
+    {$comment = test_input($_POST["comment"]);}
+
+    if (empty($_POST["gender"]))
+    {$genderErr = "Gender is required";}
+    else
+    {$gender = test_input($_POST["gender"]);}
 }
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
 ?>
 
-<h2>PHP Form Validation Example</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
     Name: <input type="text" name="name">
+    <span class="error">* <?php echo $nameErr;?></span>
     <br><br>
-    E-mail: <input type="text" name="email">
+    E-mail:
+    <input type="text" name="email">
+    <span class="error">* <?php echo $emailErr;?></span>
     <br><br>
-    Website: <input type="text" name="website">
+    Website:
+    <input type="text" name="website">
+    <span class="error"><?php echo $websiteErr;?></span>
     <br><br>
-    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-    <br><br>
-    Gender:
-    <input type="radio" name="gender" value="female">Female
-    <input type="radio" name="gender" value="male">Male
-    <br><br>
-    <input type="submit" name="submit" value="Submit">
+    <label>Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <span class="error">* <?php echo $genderErr;?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+
 </form>
 
 <?php
@@ -75,11 +118,7 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
-
-/*我们在执行以上脚本时，会通过$_SERVER["REQUEST_METHOD"]来检测表单是否被提交 。
-如果 REQUEST_METHOD 是 POST, 表单将被提交 - 数据将被验证。如果表单未提交将跳过验证并显示空白。
-在以上实例中使用输入项都是可选的，即使用户不输入任何数据也可以正常显示*/
-
 ?>
+
 </body>
 </html>
