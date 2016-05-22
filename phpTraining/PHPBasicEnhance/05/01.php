92744f6b4f8c6736c4a8aeaa547de7f065361198
poetries <jingguanliuye@gmail.com>
2016-05-22 23:15:18 +0800

    update

--- phpTraining/PHPBasicEnhance/05/01.php
+++ phpTraining/PHPBasicEnhance/05/01.php
@@ -6,3 +6,20 @@
  * Time: 15:52
  */
 
+echo time();//返回时间戳
+echo "<br>";
+
+print_r(microtime(true));
+
+echo "<br>";
+
+$start = microtime(true);
+for($i = 1;$i<105760000;$i++)
+{
+    $temp = 125777625689/46751;
+}
+
+
+$end = microtime(true);
+//检测页面开始和结束 执行的时间 时间戳用来计算程序运行时间
+echo $end - $start;
