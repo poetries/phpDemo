61d18acc44e69237c5f7944224b308af76c220f7
poetries <jingguanliuye@gmail.com>
2016-05-23 10:31:27 +0800

    update

--- phpTraining/PHPBasicEnhance/09/07.php
+++ phpTraining/PHPBasicEnhance/09/07.php
@@ -31,10 +31,21 @@ print_r($_GET);
 print_r($_POST);
 print_r($_REQUEST);
 
-print_r($_ENV); //env容易暴露服务器信息 一般不允许显示 在php ini里面修改
-print_r($_SERVER); //获取当前网站的域名以及当前访问的脚本 客户IP（REMOTE_ADDR,HTTP_X_FORWARD_FOR）
+//print_r($_ENV); //env容易暴露服务器信息 一般不允许显示 在php ini里面修改
+//print_r($_SERVER); //获取当前网站的域名以及当前访问的脚本 客户IP（REMOTE_ADDR,HTTP_X_FORWARD_FOR）
 
+//$GLABLES是对全局变量花名册的一个别名 通过该变量可以任意访问全局变量
 
+echo "<hr>";
+$a = 3;
+$b = 4;
+function t(){
+    print_r($GLOBALS);
+    $GLOBALS['a'] = 99;
+}
+
+t();
+echo $a;//99
 
 
 ?>
