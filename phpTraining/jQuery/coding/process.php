c601075e1e01740fb73d912ce619dce45919bcd1
poetries <jingguanliuye@gmail.com>
2016-06-22 20:31:40 +0800

    update

--- phpTraining/jQuery/coding/process.php
+++ phpTraining/jQuery/coding/process.php
@@ -6,9 +6,12 @@ header('Content-type:text/html;charset=utf-8');
 $username = $_GET['username'];
 
 if($username == 'admin'){
-	echo '用户名不能是admin';
+	// echo '用户名不能是admin';
+	//后台一般这样写 只返回布尔值 需要在页面中判断
+	echo "true";
 }else {
-	echo '用户名可以注册';
+	// echo '用户名可以注册';
+	echo "false";
 }
 
  ?>
