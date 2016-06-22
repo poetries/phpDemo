d1353e6d90b55204aba76aaf94a45fcbb48080b5
poetries <jingguanliuye@gmail.com>
2016-06-22 18:58:21 +0800

    update

--- phpTraining/jQuery/coding/process.php
+++ phpTraining/jQuery/coding/process.php
@@ -2,7 +2,8 @@
 
 header('Content-type:text/html;charset=utf-8');
 
-$username = $_POST['username'];
+// $username = $_POST['username'];
+$username = $_GET['username'];
 
 if($username == 'admin'){
 	echo '用户名不能是admin';
