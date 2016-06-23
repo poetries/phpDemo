b613a753a38fb325a2045966e62ef25aebff554f
poetries <jingguanliuye@gmail.com>
2016-06-23 10:01:45 +0800

    update

---
+++ phpTraining/jQuery/coding/demo1.js
@@ -0,0 +1,10 @@
+$.yongxuan={
+	centerDiv:function(obj){
+
+		obj.css({
+			'top':($(window).height()-div.height())/2,
+			'left':($(window).width()-div.width())/2,
+			'position':'absolute'
+		});
+	}
+}
