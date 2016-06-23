c6547f0edb8bb27e76d7bab05817ed151094368e
poetries <jingguanliuye@gmail.com>
2016-06-23 11:00:26 +0800

    update

---
+++ phpTraining/jQuery/coding/jquery.tab.1.0.js
@@ -0,0 +1,18 @@
+;(function($){
+	$.fn.table=function(options){
+		var defaults = {
+			//各种参数 各种属性
+			
+			
+
+
+		}
+		var options = $.extend(defaults,options);//options继承了defaults中的属性
+
+		this.each(function(){
+			//实现功能的代码
+		
+
+		});
+	}
+})(jQuery);  
--- phpTraining/jQuery/coding/jquery.table.1.0.js
+++ phpTraining/jQuery/coding/jquery.table.1.0.js
@@ -4,7 +4,12 @@
 			//各种参数 各种属性
 			evenRowClass:'evenRow',
 			oddRowClass:'oddRow',
-			currentClass:'currentRow'
+			currentClass:'currentRow',
+			eventType1:'mouseover',
+			eventType2:'mouseout',
+			
+
+
 		}
 		var options = $.extend(defaults,options);//options继承了defaults中的属性
 
@@ -17,12 +22,31 @@
 			//奇数行
 			_this.find('tr:odd').addClass(options.oddRowClass);
 
-			//鼠标放上去和鼠标离开
-			_this.find('tr').mouseover(function() {
+			//鼠标放上去和鼠标离开  实际开发中不用这种
+/*			_this.find('tr').mouseover(function() {
 				$(this).addClass(options.currentClass);
 			}).mouseout(function() {
 				$(this).removeClass(options.currentClass);
+			});*/
+
+			//bind放可以自由修改 灵活  这里写死了
+/*			_this.find('tr').bind('mouseover',function(){
+				$(this).addClass(options.currentClass);
 			});
+			_this.find('tr').bind('mouseout',function(){
+				$(this).removeClass(options.currentClass);
+			});
+*/
+
+	//下面是灵活的写法
+			_this.find('tr').bind(options.eventType1,function(){
+				$(this).addClass(options.currentClass);
+			});
+			_this.find('tr').bind(options.eventType2,function(){
+				$(this).removeClass(options.currentClass);
+			});
+
+
 		});
 	}
 })(jQuery);  
